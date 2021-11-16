59cf51a7e0e16f02413e8143366d056573b60488
Lingfeng Fu <2871618714@qq.com>
2021-11-16 17:22:35 +0800

    parsing command line argument using local encoding

--- borderless/main.cpp
+++ borderless/main.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
-	MainWindow w(nullptr, argc > 1? argv[1] : "");
+	MainWindow w(nullptr, argc > 1? QString::fromLocal8Bit(argv[1]) : "");
 	w.show();
 	return a.exec();
 }
